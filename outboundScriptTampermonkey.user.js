4b73c2d074cb37eff7a6ea3239c97adce4bd5e56
Jhonny Lima Santos <73124955+Jhoni23@users.noreply.github.com>
2025-11-04 21:38:45 -0300

    Refactor script to apply Cloudscape design directly
    Removed external script requirement and added Cloudscape design application logic.

--- outboundScriptTampermonkey.user.js
+++ outboundScriptTampermonkey.user.js
@@ -12,7 +12,6 @@
 // @connect      github.com
 // @connect      raw.githubusercontent.com
 // @require      https://sdk.amazonaws.com/js/aws-sdk-2.1480.0.min.js
-// @require      https://github.com/Jhoni23/Outbound/raw/refs/heads/new-version/cloudscape-design.js
 // ==/UserScript==
 
 (function () {
@@ -98,78 +97,389 @@
         }
     });
 
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; switch=`);
-    if (parts.length === 1) {document.cookie = "switch=false; path=/";}
+    // CLOUDSCAPE DESIGN
 
-    function criaSwitch() {
-        if (document.getElementById("switch")) return;
-        const emailSpan = document.querySelector('.a-color-link.a-text-bold');
+    function aplicarCloudscapeDesign() {
+        const css = `
+  #legacy .legacyBody {
+    font-family: "Amazon Ember", sans-serif !important;
+  }
+`;
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(css));
+        document.head.appendChild(style);
 
-        const switchLabel = document.createElement('label');
-        switchLabel.id = "switch";
-        switchLabel.style.display = "inline-flex";
-        switchLabel.style.alignItems = "center";
-        switchLabel.style.cursor = "pointer";
-        switchLabel.style.marginRight = "8px";
-        switchLabel.style.fontWeight = "100";
+        //Remove Elementos
+        document.querySelectorAll('div.floatL.topHelpLinks, span.floatL.relatedUI, #topPaneContent span.floatL.textBold')
+            .forEach(el => el.remove());
 
-        const switchInput = document.createElement('input');
-        switchInput.type = "checkbox";
-        switchInput.style.display = "none";
-        const switchSlider = document.createElement('span');
-        switchSlider.style.width = "35px";
-        switchSlider.style.height = "20px";
-        switchSlider.style.background = "none";
-        switchSlider.style.border = "2px #c5c5c5 solid";
-        switchSlider.style.borderRadius = "20px";
-        switchSlider.style.position = "relative";
-        switchSlider.style.transition = "background 0.3s";
-        const knob = document.createElement('span');
-        knob.style.width = "14px";
-        knob.style.height = "14px";
-        knob.style.background = "#c5c5c5";
-        knob.style.borderRadius = "50%";
-        knob.style.position = "absolute";
-        knob.style.top = "1px";
-        knob.style.left = "2px";
-        knob.style.transition = "left 0.3s";
-        knob.textContent = "✦";
-        knob.style.display = "flex";
-        knob.style.alignItems = "center";
-        knob.style.justifyContent = "center";
-        knob.style.fontSize = "13px";
-        knob.style.color = "#fff";
-        switchSlider.appendChild(knob);
-        switchLabel.appendChild(switchInput);
-        switchLabel.appendChild(switchSlider);
-
-        switchInput.addEventListener("change", () => {
-            if (switchInput.checked) {
-                document.cookie = "switch=true; path=/";
-                switchSlider.style.border = "2px #006ce0 solid";
-                knob.style.background = "#006ce0";
-                knob.style.left = "15px";
-            } else {
-                document.cookie = "switch=false; path=/";
-                switchSlider.style.border = "2px #c5c5c5 solid";
-                knob.style.background = "#c5c5c5";
-                knob.style.left = "2px";
-                location.reload();
+        const statusParaRemover = [
+            "READY_FOR_LOADING",
+            "LOADING_IN_PROGRESS",
+            "TRAILER_ATTACHED"
+        ];
+        document.querySelectorAll('div.originalStatusCheck').forEach(div => {
+            const status = div.getAttribute("data-status");
+            if (statusParaRemover.includes(status)) {
+                div.remove();
             }
         });
 
-        emailSpan.parentNode.insertBefore(switchLabel, emailSpan);
+        document.querySelectorAll('td div[title][alt]').forEach(div => {
+            if (div.textContent.trim() === '[ATS_CONTRACTED]') {
+                div.remove();
+            }
+        });
 
-        let value = `; ${document.cookie}`.split(`; switch=`);
-        value = value.pop().split(';').shift();
-        if (value === "true") {
-            switchSlider.style.border = "2px #006ce0 solid";
-            knob.style.background = "#006ce0";
-            knob.style.left = "15px";
-            document.querySelector('#switch input[type="checkbox"]').checked = true;
+        document.querySelectorAll('span.trailerNo').forEach(span => {
+            if (span.textContent.includes('OTHR')) {
+                span.textContent = span.textContent.replace('OTHR', '').trim();
+            }
+        });
+
+        const tabela = document.querySelector('table.dataTable');
+        if (!tabela) return;
+
+        //Departing Loads
+        const departLoads = document.querySelector('.pageHeader');
+        if (departLoads) {
+            departLoads.style.fontSize = '28px';
+            departLoads.style.fontWeight = '400';
+            departLoads.style.color = '#232F3E';
+        }
+
+        //GRU8 Select
+        const nodeTimeDiv = document.querySelector('.floatL.nodeTime');
+        if (nodeTimeDiv) {
+            nodeTimeDiv.style.marginTop = '5px';
+        }
+        const selectEl = document.getElementById('availableNodeName');
+        if(selectEl) {
+            selectEl.style.height = '28px';
+            selectEl.style.backgroundColor = '#ffffff';
+            selectEl.style.border = '1px solid #6e777f';
+            selectEl.style.borderRadius = '4px';
+            selectEl.style.padding = '0px 8px';
+            selectEl.style.fontSize = '12px';
+            selectEl.style.color = '#575F67';
+        }
+        const departDiv = document.querySelector('.pageHeaderNode');
+        if (departDiv) {
+            departDiv.classList.remove('marL20');
+            departDiv.style.marginLeft = '40px';
+        }
+
+        //Refresh
+        const el = document.getElementById('manualRefresh');
+        if (el) {
+            el.classList.remove('ui-icon-refresh', 'floatR');
+            el.innerHTML = '⟳';
+            el.style.lineHeight = '1.1';
+            el.style.fontSize = '35px';
+            el.style.color = '#00688D';
+            el.style.cursor = 'pointer';
+            el.style.marginLeft = '5px';
+        }
+        const man = document.querySelector('.refreshButton');
+        if (man) {
+            man.style.paddingRight = '25px';
+        }
+
+        // Tamanho do texto
+        tabela.querySelectorAll('th, td').forEach(cell => {
+            cell.style.fontSize = '12px';
+        });
+
+        //Botões superiores
+        const botoes = document.querySelectorAll('.topButtonLinks input[type="button"]');
+        botoes.forEach((input) => {
+            input.style.boxSizing = 'border-box';
+            input.style.fontSize = '14px';
+            input.style.fontWeight = '400';
+            input.style.setProperty("padding", "0px 16px", "important");
+            input.style.fontFamily = "Amazon Ember";
+            input.style.minWidth = '100px';
+            input.style.textAlign = 'center';
+            input.style.height = '32px';
+            input.style.display = 'inline-block';
+            input.style.borderRadius = '4px';
+            input.style.border = '2px solid #00688D';
+            input.style.color = '#00688D';
+            input.style.backgroundColor = 'transparent';
+        });
+        const botaoRefresh = document.getElementById('disableRefresh');
+        if (botaoRefresh) {
+            botaoRefresh.style.minWidth = '160px';
+            botaoRefresh.style.width = 'auto';
+        }
+
+        //Bordas divs superiores
+        document.querySelectorAll('.topDetailPane table').forEach(table => {
+            table.removeAttribute('border');
+            table.style.border = 'none';
+        });
+        const topDetail = document.querySelector('#topDetailList');
+        if(topDetail){
+            topDetail.style.display = 'flex';
+            topDetail.style.gap = '10px';
+            topDetail.style.flexWrap = 'nowrap';
+            topDetail.style.alignItems = 'stretch';
+            document.querySelectorAll('#topDetailList > div').forEach(div => {
+                div.style.height = '190px';
+                div.style.borderRadius = '4px';
+                div.style.border = '1px solid #B1BAC3';
+                div.style.padding = '20px';
+                div.style.display = 'flex';
+                div.style.flexDirection = 'column';
+                div.style.backgroundColor = '#FFFFFF';
+            });
+        }
+
+        //Next CPT
+        const nextCPT = document.querySelector('#nextPrevCptData');
+        if(nextCPT) {nextCPT.style.backgroundColor = '#FFFFFF'};
+        const spans = document.querySelectorAll('#nextPrevCptData span');
+        spans.forEach(span => {
+            if (span.textContent.trim() === 'Next CPT' || span.textContent.trim() === 'Próximo CPT') {
+                span.style.fontSize = '18px';
+                span.style.fontWeight = '700';
+                span.style.color = '#0f141a';
+                span.style.marginBottom = '8px';
+            }
+        });
+        document.querySelectorAll("#nextCPT, #prevCPT").forEach(el => {
+            el.classList.remove("standardBtn");
+        });
+
+        //Load Issues
+        const loaDiv = document.querySelector('div.col-md-4.topDetailPane');
+        const load = document.querySelector('div.col-md-4.topDetailPane table tbody tr td.loadHead');
+        if (loaDiv) {
+            loaDiv.style.backgroundColor = '#FFFFFF';
+            loaDiv.classList.remove("col-md-4");
+            loaDiv.style.width = '33%';
+        }
+        if (load) {
+            load.classList.remove("col-md-4");
+            load.childNodes.forEach(node => {
+                if (node.nodeType === Node.TEXT_NODE) {
+                    if (node.textContent.includes('Load Issues') || node.textContent.includes('Problemas de carregamento')) {
+                        const span = document.createElement('span');
+                        span.textContent = 'Problemas de Carga ';
+                        span.style.fontSize = '18px';
+                        span.style.fontWeight = '700';
+                        span.style.color = '#0f141a';
+                        load.replaceChild(span, node);
+                    }
+                }
+            });
+        }
+        const aView = document.querySelector('a#viewAlertTable');
+        if (aView) {
+            aView.classList.remove("aluiBtn", "standardBtn");
+            aView.style.boxSizing = 'border-box';
+            aView.style.paddingTop = '8px';
+            aView.style.marginLeft = '10px';
+            aView.style.minWidth = '100px';
+            aView.style.textAlign = 'center';
+            aView.style.height = '27px';
+            aView.style.display = 'inline-block';
+            aView.style.borderRadius = '4px';
+            aView.style.border = '2px solid #00688D';
+            aView.style.color = '#00688D';
+            aView.style.backgroundColor = 'transparent';
+        }
+        document.querySelectorAll(".lateDetailsHeight").forEach(el => {
+            el.style.margin = "0px 0px 10px 0px";
+        });
+        document.querySelectorAll(".topPaneFontBig").forEach(el => {
+            el.classList.remove('topPaneFontBig');
+            el.style.fontSize = "40px";
+        });
+        document.querySelectorAll(".alertBg1").forEach(el => {
+            el.style.backgroundColor = "transparent";
+            el.style.color = "#666666";
+            const span = el.querySelector("span");
+            if (span) span.style.color = "#f2cd54";
+        });
+        document.querySelectorAll(".alertBg2").forEach(el => {
+            el.style.backgroundColor = "transparent";
+            el.style.color = "#666666";
+            const span = el.querySelector("span");
+            if (span) span.style.color = "#db0000";
+        });
+
+        //Notification
+        document.querySelectorAll('.specialEventsDiv').forEach(el => {
+            el.classList.remove('specialEventsDiv');
+            el.classList.remove('col-md-4');
+            el.style.width = '36%';
+            el.style.padding = '20px';
+            el.style.textAlign = 'left';
+            if (el.firstElementChild) {
+                el.firstElementChild.style.backgroundColor = "transparent";
+                el.firstElementChild.style.paddingBottom = "10px";
+                el.firstElementChild.style.border = "none";
+                const span = el.firstElementChild.querySelector('span');
+                if (span) {
+                    span.textContent = "Notificações";
+                    span.style.fontSize = '19px';
+                    span.style.fontWeight = '700';
+                    span.style.color = '#0f141a';
+                    span.style.padding = '0px';
+                }
+            }
+        });
+
+        //Overdue Packages
+        const td = document.getElementById('selectedUt');
+        if (td) {
+            const divPai = td.closest("div.col-md-1.topDetailPane");
+            if (divPai) {
+                divPai.style.width = '10%';
+                divPai.style.backgroundColor = "#FFFFFF";
+            }
+        };
+        if (td && !td.innerHTML.includes('<br>Packages')) {
+            td.innerHTML = td.innerHTML.replace('Packages', '<br>Packages');
+            td.classList.remove('textCenter');
+        }
+        const overDueDiv = document.getElementById('overDueCount');
+        if (overDueDiv) {
+            overDueDiv.style.fontSize = '38px';
+            overDueDiv.style.fontWeight = '700';
+            //overDueDiv.style.color = '#006ce0';
+        }
+        const titulo = document.getElementById('selectedUt');
+        if (titulo) {
+            const textoSpan = document.createElement('span');
+            textoSpan.textContent = 'Pacotes Atrasados';
+            textoSpan.style.fontSize = '18px';
+            textoSpan.style.fontWeight = '700';
+            textoSpan.style.display = 'block';
+            textoSpan.style.marginBottom = '25px';
+            textoSpan.style.color = '#0f141a';
+
+            const overDueDiv = titulo.querySelector('#overDueCount');
+            titulo.innerHTML = '';
+            titulo.appendChild(textoSpan);
+            if (overDueDiv) {
+                titulo.appendChild(overDueDiv);
+            }
+        }
+
+        //TABELA
+        tabela.style.border = 'none';
+        let container = tabela.parentElement;
+        container.style.overflow = 'hidden';
+        container.style.borderRadius = '4px';
+        container.style.border = '1px solid #c6c6cd';
+        container.style.padding = '20px';
+        container.style.backgroundColor = '#FFFFFF';
+
+        const search = document.querySelector('input.filterSearchLoads');
+        if (search) {
+            search.style.appearance = "none";
+            search.style.MozAppearance = "none";
+            search.style.height = "30px";
+            search.style.lineHeight = "30px";
+            search.style.padding = "0 10px";
+            search.style.border = "1px solid #8c8c94";
+            search.style.borderRadius = "6px";
+            search.style.boxSizing = "border-box";
+            search.placeholder = "Pesquisar";
+            search.style.boxShadow = "none";
+            search.style.marginTop = "0px";
+        };
+
+        const conf = document.querySelector('button#alui-columnToggle-btn');
+        if (conf) {
+            conf.style.backgroundColor = '#fff';
+            conf.style.border = "1px solid #DDDDDD";
+            conf.style.height = "30px";
+        };
+
+        const selectPage = document.querySelector('select[name="dashboard_length"]');
+        if (selectPage) {
+            selectPage.style.setProperty('background-color', '#fff', 'important');
+            selectPage.style.height = '24px';
+            selectPage.style.border = '1px solid #DDDDDD';
+            selectPage.style.borderRadius = '8px';
+        }
+
+        const label = document.querySelector('label[for="dashboard_length"]')
+        || document.querySelector('label:has(select[name="dashboard_length"])');
+        if (label) {
+            if (label.childNodes[0]) label.childNodes[0].textContent = "";
+            if (label.lastChild) label.lastChild.textContent = " Linhas";
+        }
+
+        document.querySelectorAll(
+            '.alui-skin .tabletoolbar .dataTables_paginate .previous.ui-button, ' +
+            '.alui-skin .tabletoolbar .dataTables_paginate .next.ui-button'
+        ).forEach(el => {
+            el.style.setProperty('border', 'none', 'important');
+        });
+
+        //CABEÇALHO
+        tabela.querySelectorAll('thead tr:first-child th').forEach(th => {
+            th.style.fontSize = '14px';
+            th.style.lineHeight = '18px';
+            th.style.fontWeight = 'Bold';
+            th.style.letterSpacing = 'normal';
+            th.style.border = 'none';
+            th.style.backgroundColor = '#fff';
+            th.style.color = '#424650';
+
+            if (th.textContent.includes('Doca') || th.textContent.includes('Local')) {
+                th.style.width = '8%';
+            }
+        });
+
+        // Linhas cinzas
+        document.querySelectorAll('tr.even').forEach(tr => {
+            tr.classList.remove('even');
+            tr.classList.add('odd');
+        });
+
+        //Linhas grupos
+        document.querySelectorAll('td.group').forEach(tdGroup => {
+            tdGroup.style.background = "#F7F7F7";
+            tdGroup.style.lineHeight = "28px";
+            const span = tdGroup.firstElementChild.querySelector('span');
+            if (span) {
+                span.style.fontWeight = '200';
+                span.style.color = '#000000';
+            }
+        });
+
+        //Status
+        document.querySelectorAll(".dwellProgressStatusTime").forEach(el => {
+            let texto = el.textContent.trim();
+            if (texto.includes("Since")) {
+                el.textContent = texto.replace("Since", "Desde");
+            } else if (texto.includes(" Hour")) {
+                el.textContent = texto.replace(" Hour", "h");
+            }
+        });
+        document.querySelectorAll('[data-status="SCHEDULED"]').forEach(el => {
+            if (el.textContent.trim() === "Scheduled") {
+                el.textContent = "Programado";
+            }
+        });
+
+        //Div Right
+        const rightContent = document.getElementById("rightContent");
+        if (rightContent) {
+            rightContent.style.borderRadius = "16px";
+            rightContent.style.border = "1px solid #c6c6cd";
+            rightContent.style.paddingTop = "20px";
+            rightContent.style.fontSize = '14px';
         }
     }
+
+    // FIM CLOUDSCAPE
 
     function formatarDatas() {
         const meses = {
@@ -810,7 +1120,7 @@
         });
 
         // Contagem de rota
-
+        /*
         aaData.forEach(item => {
             //console.log(item);
         });
@@ -889,16 +1199,16 @@
                 };
             });
         });
+        */
     }
 
     async function processarPagina() {
-        criaSwitch();
         selecionarFC();
         formatarDatas();
         traduzirCampos();
         adicionarBotaoValePallet();
         if (FC == "GRU8" ) {adicionarColunaMotorista();}
-        if (document.querySelector('#switch input[type="checkbox"]').checked) {aplicarCloudscapeDesign();};
+        aplicarCloudscapeDesign();
     }
 
     window.addEventListener('load', () => {
